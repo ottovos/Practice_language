0ca5dfe4c715837571f5662372e70a426dcce36f
ottovos <otto.vos@customers2you.com>
2021-11-22 17:10:01 +0100

    Added Words and Meaning to practice.html template

--- practice/views.py
+++ practice/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from . models import Language, Words
+from .models import Language, Words
 from django.http import HttpResponse
 
+
 def practiceWords(request):
-    all_words_list = Words.objects.order_by()
+    all_words_list = Words.objects.all();
     context = {'all_words_list': all_words_list}
     return render(request, 'practice/practice.html', context)
+
 
 def addWords(request):
     # return HttpResponse('Later you will find here the choice of language to practice with')
     return render(request, 'practice/addWords.html')
-
-
-
